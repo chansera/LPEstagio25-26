30361b82da9d857a1aff49b33df42bf40684e840
Csrand <chrystiansenna@gmail.com>
2026-02-02 11:06:29 -0300

    feat: Button Hover

--- app/src/lib/components/atoms/Button.svelte
+++ app/src/lib/components/atoms/Button.svelte
@@ -6,7 +6,7 @@
   const baseClass = "font-semibold py-2 px-6 sm:py-3 sm:px-8 rounded-lg text-base sm:text-lg shadow-lg hover:shadow-xl hover:-translate-y-1 transition-all duration-300 inline-flex items-center justify-center";
 
   const variantClasses = {
-    primary: "bg-black text-white hover:bg-brand-dark",
+    primary: "bg-black text-white hover:bg-brand",
     secondary: "bg-gray-200 text-gray-800 hover:bg-gray-300",
     outline: "border-2 border-brand-green text-brand-green hover:bg-brand-green hover:text-white"
   };
