4b43b4ef3ee0f9e358e5f8bf61819f3507bcb219
Csrand <chrystiansenna@gmail.com>
2026-02-27 10:22:17 -0300

    chore: recomeço

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-import Hero from '$lib/components/molecules/Hero.svelte';
-import Cta from '$lib/components/molecules/Cta.svelte';
- import Features from '$lib/components/molecules/Features.svelte';
 </script>
 <svelte:head>
     <title>DAA Aviação - Manutenção de Componentes Aeronáuticos Homologada ANAC, FAA, EASA</title>
@@ -9,6 +6,3 @@ import Cta from '$lib/components/molecules/Cta.svelte';
     <meta name="keywords" content="manutenção aeronáutica, componente aeronáutico, ANAC, FAA, EASA, oficina homologada, manutenção de aeronaves" />
     <meta name="robots" content="index, follow" />
 </svelte:head>
-<Hero />
-<Cta />
-<Features />
